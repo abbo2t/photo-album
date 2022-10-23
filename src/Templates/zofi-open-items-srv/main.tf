6724a3ca948dfb7f7dbfc3df7304682f36f4230f
WSM18I8 <abbotttylerb@johndeere.com>
2022-10-23 18:34:34 -0500

    use sap.johndeerecloud for service_url

--- src/Templates/zofi-open-items-srv/main.tf
+++ src/Templates/zofi-open-items-srv/main.tf
@@ -14,7 +14,7 @@ module "{{module-name}}_dev" {
   name                         = "{{name}}-DEV"
   display_name                 = "{{display-name}} - DEV"
   path                         = "{{path}}/dev"
-  service_url                  = "https://{{service_subdomain_dev}}.dx.deere.com:40443/sap/opu/odata/sap/{{service-url}}"
+  service_url                  = "https://{{service_subdomain_dev}}.sap.johndeerecloud.com/sap/opu/odata/sap/{{service-url}}"
   auth_server_url              = "https://sso-dev.johndeere.com/oauth2/ausi7tpzliZSTWOp10h7/.well-known/oauth-authorization-server"
   cc_to_sap_list = []
   sap_client_id = {{sap_client_id_dev}}
@@ -33,7 +33,7 @@ module "{{module-name}}_qual" {
   name                         = "{{name}}-QUAL"
   display_name                 = "{{display-name}} - QUAL"
   path                         = "{{path}}/qual"
-  service_url                  = "https://{{service_subdomain_qual}}.dx.deere.com:40443/sap/opu/odata/sap/{{service-url}}"
+  service_url                  = "https://{{service_subdomain_qual}}.sap.johndeerecloud.com/sap/opu/odata/sap/{{service-url}}"
   auth_server_url              = "https://sso-qual.johndeere.com/oauth2/ausi350kyb3DS4mMI0h7/.well-known/oauth-authorization-server"
   cc_to_sap_list = []
   sap_client_id = {{sap_client_id_qual}}
@@ -53,7 +53,7 @@ module "{{module-name}}_cert" {
   name                         = "{{name}}-CERT"
   display_name                 = "{{display-name}} - CERT"
   path                         = "{{path}}/cert"
-  service_url                  = "https://{{service_subdomain_cert}}.dx.deere.com:40443/sap/opu/odata/sap/{{service-url}}"
+  service_url                  = "https://{{service_subdomain_cert}}.sap.johndeerecloud.com/sap/opu/odata/sap/{{service-url}}"
   auth_server_url              = "https://sso-cert.johndeere.com/oauth2/aus972savuI4PAAgc1t7/.well-known/oauth-authorization-server"
   cc_to_sap_list = []
   sap_client_id = {{sap_client_id_cert}}
@@ -72,7 +72,7 @@ module "{{module-name}}_prod" {
   name                         = "{{name}}-PROD"
   display_name                 = "{{display-name}} - PROD"
   path                         = "{{path}}"
-  service_url                  = "https://{{service_subdomain_prod}}.dx.deere.com:40443/sap/opu/odata/sap/{{service-url}}"
+  service_url                  = "https://{{service_subdomain_prod}}.sap.johndeerecloud.com/sap/opu/odata/sap/{{service-url}}"
   auth_server_url              = "https://sso.johndeere.com/oauth2/aus9fatalq0bxmt361t7/.well-known/oauth-authorization-server"
   cc_to_sap_list = []
   sap_client_id = {{sap_client_id_prod}}
